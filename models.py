4f3202988c83b9a15ee94ec72d77a4915ff508fd
Myles Braithwaite <me@mylesbraithwaite.com>
2008-06-26 16:09:51 +0000

    Added the license comment to all the files.
    git-svn-id: http://django-photo-gallery.googlecode.com/svn/trunk@15 23590a6b-404f-0410-95fd-03f1236d02ac

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
+"""
+	Copyright (c) 2008, Myles Braithwaite
+	All rights reserved.
+	
+	This software is provided without warranty under the terms of the BSD
+	license included in photos/LICENSE.txt and may be redistributed only under
+	the conditions described in the aforementioned license. This license is also
+	available online at http://code.google.com/p/django-photo-gallery/wiki/License
+	
+	Author: Myles Braithwaite
+"""
+
 import os
 from PIL import Image
 
