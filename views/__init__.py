22feeb8dfd51fcb8afc0dac4eb0c0768eaaccf4c
Myles Braithwaite <me@mylesbraithwaite.com>
2008-08-11 19:34:21 +0000

    Split the __init__.py views into mutiple files.
    git-svn-id: http://django-photo-gallery.googlecode.com/svn/trunk@29 23590a6b-404f-0410-95fd-03f1236d02ac

--- views/__init__.py
+++ views/__init__.py
@@ -13,82 +13,8 @@
 from django.http import Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.core.paginator import QuerySetPaginator, InvalidPage
 
-from photos.models import Gallery, Photo, Module
-
-def gallery_index(request):
-	galleries = Gallery.objects.all()[:6]
-	favorites = Photo.objects.filter(favorite=True)[:6]
-	galleries_count = Gallery.objects.all().count()
-	photos_count = Photo.objects.all().count()
-	favorites_count = Photo.objects.filter(favorite=True).count()
-	
-	return render(request=request, template_name='photos/gallery_index.html', payload={
-		'galleries'			: galleries,
-		'favorites'			: favorites,
-		'galleries_count'	: galleries_count,
-		'photos_count'		: photos_count,
-		'favorites_count'	: favorites_count,
-	})
-
-def gallery_title(request, gallery_slug, page=0):
-	gallery = get_object_or_404(Gallery, slug__iexact=gallery_slug)
-	
-	return render(request, 'photos/gallery_title.html', { 'gallery': gallery })
-
-def gallery_detail(request, gallery_slug, page=0):
-	gallery = get_object_or_404(Gallery, slug__iexact=gallery_slug)
-	photos = Photo.objects.filter(gallery=gallery)
-	
-	paginator = QuerySetPaginator(photos, 25, allow_empty_first_page=True)
-	if not page:
-		page = request.GET.get('page', 1)
-		try:
-			page_number = int(page)
-		except ValueError:
-			if page == 'last':
-				page_number = paginator.num_pages
-			else:
-				raise Http404
-		
-		try:
-			page_obj = paginator.page(page_number)
-		except InvalidPage:
-			raise Http404
-	
-	return render(request, 'photos/gallery_detail.html', {
-		'gallery':			gallery,
-		'photos':			photos,
-		'is_paginated':		page_obj.has_other_pages(),
-		'results_per_page':	paginator.per_page,
-		'has_next':			page_obj.has_next(),
-		'has_previous':		page_obj.has_previous(),
-		'page':				page_obj.number,
-		'next':				page_obj.next_page_number(),
-		'previous':			page_obj.previous_page_number(),
-		'first_on_page':	page_obj.start_index(),
-		'last_on_page':		page_obj.end_index(),
-		'pages':			paginator.num_pages,
-		'hits':				paginator.count,
-		'page_range':		paginator.page_range,
-	})
-
-def gallery_archive(request):
-	galleries = Gallery.objects.all()
-	
-	return render(request=request, template_name='photos/gallery_archive.html', payload={
-		'galleries':	galleries,
-	})
-
-def photo_detail(request, gallery_slug, photo_slug):
-	gallery = get_object_or_404(Gallery, slug__iexact=gallery_slug)
-	photo = get_object_or_404(Photo, slug__iexact=photo_slug)
-	
-	return render(request=request, template_name='photos/photo_detail.html', payload={
-		'gallery':	gallery,
-		'photo':	photo,
-	})
+from photos.models import Module
 
 def render(request, template_name, payload):
 	footer = Module.objects.get(module_type=1)
---
+++ views/photo.py
@@ -0,0 +1,25 @@
+"""
+	Copyright (c) 2008, Myles Braithwaite
+	All rights reserved.
+	
+	This software is provided without warranty under the terms of the BSD
+	license included in photos/LICENSE.txt and may be redistributed only under
+	the conditions described in the aforementioned license. This license is also
+	available online at http://code.google.com/p/django-photo-gallery/wiki/License
+	
+	Author: Myles Braithwaite
+"""
+
+from django.shortcuts import get_object_or_404
+
+from photos.views import render
+from photos.models import Gallery, Photo
+
+def photo_detail(request, gallery_slug, photo_slug):
+	gallery = get_object_or_404(Gallery, slug__iexact=gallery_slug)
+	photo = get_object_or_404(Photo, slug__iexact=photo_slug)
+	
+	return render(request=request, template_name='photos/photo_detail.html', payload={
+		'gallery':	gallery,
+		'photo':	photo,
+	})
